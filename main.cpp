1107ece946bec7dc8ef95afa705d07d4f9f64359
likun <likun@nb-ai.com>
2018-08-18 14:25:20 +0800

    adding

--- main.cpp
+++ main.cpp
@@ -14,10 +14,13 @@
 #include <pcl/segmentation/region_growing_rgb.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/segmentation/min_cut_segmentation.h>
+#include <pcl/segmentation/conditional_euclidean_clustering.h>
+#include <pcl/console/time.h>
 using namespace std;
 typedef pcl::PointXYZ PointT;
 typedef pcl::PointXYZRGB PointRGB;
-
+typedef pcl::PointXYZI PointTypeIO;
+typedef pcl::PointXYZINormal PointTypeFull;
 
 void regiongrowing(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
 {
@@ -66,8 +69,8 @@ void regiongrowing(pcl::PointCloud<pcl::PointXYZ>::Ptr cloud)
 	std::cout << std::endl;
 
 	pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud ();
-        //writer("result.pcd", *colored_cloud);
-        pcl::io::savePCDFile("result.pcd", *colored_cloud); 
+	//writer("result.pcd", *colored_cloud);
+	pcl::io::savePCDFile("result.pcd", *colored_cloud); 
 	pcl::visualization::CloudViewer viewer ("Cluster viewer");
 	viewer.showCloud(colored_cloud);
 	while (!viewer.wasStopped ())
@@ -288,8 +291,8 @@ void color_segment(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
 	reg.extract (clusters);
 
 	pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud ();
-        pcl::PCDWriter writer;
-        writer.write("colored_cloud.pcd", *colored_cloud);
+	pcl::PCDWriter writer;
+	writer.write("colored_cloud.pcd", *colored_cloud);
 	pcl::visualization::CloudViewer viewer ("Cluster viewer");
 	viewer.showCloud (colored_cloud);
 	while (!viewer.wasStopped ())
@@ -298,7 +301,7 @@ void color_segment(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud)
 	}
 }
 
-void MinCutSegment(pcl::PointCloud<PointT> cloud)
+void MinCutSegmentation(pcl::PointCloud<PointT>::Ptr cloud)
 {
 	pcl::IndicesPtr indices (new std::vector <int>);
 	pcl::PassThrough<PointT> pass;
@@ -330,6 +333,8 @@ void MinCutSegment(pcl::PointCloud<PointT> cloud)
 	std::cout << "Maximum flow is " << seg.getMaxFlow () << std::endl;
 
 	pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = seg.getColoredCloud ();
+	pcl::PCDWriter writer;
+	writer.write("min_cut.pcd", *colored_cloud);
 	pcl::visualization::CloudViewer viewer ("Cluster viewer");
 	viewer.showCloud(colored_cloud);
 	while (!viewer.wasStopped ())
@@ -337,10 +342,120 @@ void MinCutSegment(pcl::PointCloud<PointT> cloud)
 	}
 }
 
+typedef pcl::PointXYZI PointTypeIO;
+typedef pcl::PointXYZINormal PointTypeFull;
+
+	bool
+enforceIntensitySimilarity (const PointTypeFull& point_a, const PointTypeFull& point_b, float squared_distance)
+{
+	if (fabs (point_a.intensity - point_b.intensity) < 5.0f)
+		return (true);
+	else
+		return (false);
+}
+
+	bool
+enforceCurvatureOrIntensitySimilarity (const PointTypeFull& point_a, const PointTypeFull& point_b, float squared_distance)
+{
+	Eigen::Map<const Eigen::Vector3f> point_a_normal = point_a.getNormalVector3fMap (), point_b_normal = point_b.getNormalVector3fMap ();
+	if (fabs (point_a.intensity - point_b.intensity) < 5.0f)
+		return (true);
+	if (fabs (point_a_normal.dot (point_b_normal)) < 0.05)
+		return (true);
+	return (false);
+}
+
+	bool
+customRegionGrowing (const PointTypeFull& point_a, const PointTypeFull& point_b, float squared_distance)
+{
+	Eigen::Map<const Eigen::Vector3f> point_a_normal = point_a.getNormalVector3fMap (), point_b_normal = point_b.getNormalVector3fMap ();
+	if (squared_distance < 10000)
+	{
+		if (fabs (point_a.intensity - point_b.intensity) < 8.0f)
+			return (true);
+		if (fabs (point_a_normal.dot (point_b_normal)) < 0.06)
+			return (true);
+	}
+	else
+	{
+		if (fabs (point_a.intensity - point_b.intensity) < 3.0f)
+			return (true);
+	}
+	return (false);
+}
+
+
+void conditional_euclidean(pcl::PointCloud<PointTypeIO>::Ptr cloud_in )
+{
+	// Data containers used
+	pcl::PointCloud<PointTypeIO>::Ptr cloud_out (new pcl::PointCloud<PointTypeIO>);
+	pcl::PointCloud<PointTypeFull>::Ptr cloud_with_normals (new pcl::PointCloud<PointTypeFull>);
+	pcl::IndicesClustersPtr clusters (new pcl::IndicesClusters), small_clusters (new pcl::IndicesClusters), large_clusters (new pcl::IndicesClusters);
+	pcl::search::KdTree<PointTypeIO>::Ptr search_tree (new pcl::search::KdTree<PointTypeIO>);
+	pcl::console::TicToc tt;
+
+	// Load the input point cloud
+	std::cerr << "Loading...\n", tt.tic ();
+	pcl::io::loadPCDFile ("Statues_4.pcd", *cloud_in);
+	std::cerr << ">> Done: " << tt.toc () << " ms, " << cloud_in->points.size () << " points\n";
+
+	// Downsample the cloud using a Voxel Grid class
+	std::cerr << "Downsampling...\n", tt.tic ();
+	pcl::VoxelGrid<PointTypeIO> vg;
+	vg.setInputCloud (cloud_in);
+	vg.setLeafSize (80.0, 80.0, 80.0);
+	vg.setDownsampleAllData (true);
+	vg.filter (*cloud_out);
+	std::cerr << ">> Done: " << tt.toc () << " ms, " << cloud_out->points.size () << " points\n";
+
+	// Set up a Normal Estimation class and merge data in cloud_with_normals
+	std::cerr << "Computing normals...\n", tt.tic ();
+	pcl::copyPointCloud (*cloud_out, *cloud_with_normals);
+	pcl::NormalEstimation<PointTypeIO, PointTypeFull> ne;
+	ne.setInputCloud (cloud_out);
+	ne.setSearchMethod (search_tree);
+	ne.setRadiusSearch (300.0);
+	ne.compute (*cloud_with_normals);
+	std::cerr << ">> Done: " << tt.toc () << " ms\n";
+
+	// Set up a Conditional Euclidean Clustering class
+	std::cerr << "Segmenting to clusters...\n", tt.tic ();
+	pcl::ConditionalEuclideanClustering<PointTypeFull> cec (true);
+	cec.setInputCloud (cloud_with_normals);
+	cec.setConditionFunction (&customRegionGrowing);
+	cec.setClusterTolerance (500.0);
+	cec.setMinClusterSize (cloud_with_normals->points.size () / 1000);
+	cec.setMaxClusterSize (cloud_with_normals->points.size () / 5);
+	cec.segment (*clusters);
+	cec.getRemovedClusters (small_clusters, large_clusters);
+	std::cerr << ">> Done: " << tt.toc () << " ms\n";
+
+	// Using the intensity channel for lazy visualization of the output
+	for (int i = 0; i < small_clusters->size (); ++i)
+		for (int j = 0; j < (*small_clusters)[i].indices.size (); ++j)
+			cloud_out->points[(*small_clusters)[i].indices[j]].intensity = -2.0;
+	for (int i = 0; i < large_clusters->size (); ++i)
+		for (int j = 0; j < (*large_clusters)[i].indices.size (); ++j)
+			cloud_out->points[(*large_clusters)[i].indices[j]].intensity = +10.0;
+	for (int i = 0; i < clusters->size (); ++i)
+	{
+		int label = rand () % 8;
+		for (int j = 0; j < (*clusters)[i].indices.size (); ++j)
+			cloud_out->points[(*clusters)[i].indices[j]].intensity = label;
+	}
+
+	// Save the output point cloud
+	std::cerr << "Saving...\n", tt.tic ();
+	pcl::io::savePCDFile ("output.pcd", *cloud_out);
+	std::cerr << ">> Done: " << tt.toc () << " ms\n";
+}
+
+
+
 int
 main (int argc, char** argv)
 {
-	pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>);
+	pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>());
         //pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 	pcl::PCDReader reader;
         reader.read (argv[1], *cloud);
